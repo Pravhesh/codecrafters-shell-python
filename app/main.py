8b7130f14c10f41b78c3269f46be602bc4bc8ce1
Pravhesh V <praavesh.v@gmail.com>
2025-02-01 19:08:48 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -4,10 +4,11 @@ import sys
 def main():
     # Uncomment this block to pass the first stage
     
-    sys.stdout.write("$ ")
+    
 
     # Wait for user input
     while(True):
+        sys.stdout.write("$ ")
         command = input()
         print(f"{command}: command not found")
 
