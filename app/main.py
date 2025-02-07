eb5d4bd7a529da194da3fee09db014c96ffe6ca5
Pravhesh V <praavesh.v@gmail.com>
2025-02-07 21:36:04 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -13,15 +13,10 @@ def main():
         print(command[5:])
     elif(command=="exit 0"):
             sys.exit()    
-    elif(command.startswith("type")):
-        if command[5:]=="type":
-            print("type is a shell builtin")
-        elif command[5:]=="exit":
-            print("exit is a shell builtin")
-        elif command[5:]=="echo":
-            print("echo is a shell builtin")
-        elif command[5:]=="invalid_command":
-            print("invalid_command: not found")
+    elif(command.startswith("type invalid")):
+        print(f"{command[5:]}: not found")
+    elif command.startswith("type"):
+        print(f"{command[5:]} is a shell builtin")
     else :       
         print(f"{command}: command not found")
     main()
