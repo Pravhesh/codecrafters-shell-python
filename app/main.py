460d292b70fa4c7da54308a2195d464ebf66c9f4
Pravhesh V <praavesh.v@gmail.com>
2025-02-07 21:45:58 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import sys
-
+import os
 
 def main():
     # Uncomment this block to pass the first stage
@@ -7,7 +7,7 @@ def main():
     sys.stdout.write("$ ")
 
     # Wait for user input
-    
+    PATH=os.getenv("PATH").split(":")
     command = input()
     if command.startswith("echo"):
         print(command[5:])
