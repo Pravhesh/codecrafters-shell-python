6aa7ccfc69ff3a3c7bb3a868830f0ebc0e993083
Pravhesh V <praavesh.v@gmail.com>
2025-02-08 22:14:17 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -37,7 +37,7 @@ def main():
         print(os.getcwd())
     elif(command.startswith("cd")):
         try:
-            os.chdir(" ".join(command[1:]))
+            os.chdir(" ".join(command[2:]))
         except FileNotFoundError:
             print(" ".join(command) + ": No such file or directory")
     else:
