848d666612e8f1d81e6ec3c2c1ce43caa2996e72
Pravhesh V <praavesh.v@gmail.com>
2025-02-08 21:53:28 +0530

    [any message]

--- app/main.py
+++ app/main.py
@@ -9,19 +9,21 @@ def main():
     # Uncomment this block to pass the first stage
     
     sys.stdout.write("$ ")
+    sys.stdout.flush()
+
 
     # Wait for user input
     PATH=os.getenv("PATH").split(":")
     builtin_cmd=["echo","exit","type"]
-    command= input().split(" ")
+    command,args= input().strip()
+    executable = args[0]
     
-    if command[0]==("echo"):
+    if command.startswith("echo"):
         print(command[5:])
-    if os.path.isfile(command[0]):
-                    os.system(command)
+
     elif(command=="exit 0"):
             sys.exit()    
-    elif command[0]==("type"):
+    elif command.startswith("type"):
         cmd_path=None
         for path in PATH :
             if os.path.isfile(f"{path}/{command[5:]}"):
@@ -30,8 +32,23 @@ def main():
             print(f"{command[5:]} is a shell builtin")
         elif cmd_path:
             print(cmd_path)
-        elif(command[0]==("type invalid")):
+        elif(command.startswith("type invalid")):
             print(f"{command[5:]}: not found")
+    elif executable.startswith("custom"):
+            executable_path = None
+            for path in PATH:
+                full_path = os.path.join(path, executable)
+                if os.path.isfile(full_path) and os.access(full_path, os.X_OK):
+                    executable_path = full_path
+                    break
+            if executable_path:
+                try:
+                    # Run the external command and capture its output
+                    subprocess.run(args)
+                except Exception as e:
+                    print(f"Error: {e}")
+            else:
+                print(f"{executable}: command not found")
     else :       
         print(f"{command}: command not found")
     main()
